5334706860c7af75dc467e0506dadee9528a6513
Enquil <jim.e.olesen@gmail.com>
2023-01-25 02:09:56 +0000

    add summernote

--- rtr/urls.py
+++ rtr/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 
 urlpatterns = [
     path('', include('newssite.urls')),
     path('admin/', admin.site.urls),
+    path('summernote/', include('summernote.urls'))
 ]
