76b881fcadbfaa108af2c128a6ad0903fd25334a
Enquil <jim.e.olesen@gmail.com>
2023-01-25 16:46:02 +0000

    create classview categoryList

--- newssite/views.py
+++ newssite/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views import generic, View
-from .models import Post
+from .models import Post, CATEGORY_CHOICES
 
 
 def index(request):
@@ -13,3 +13,8 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 4
+
+
+class CategoryList(generic.ListView):
+    model = CATEGORY_CHOICES
+    queryset = CATEGORY_CHOICES
