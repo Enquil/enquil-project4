5aaa27a305a2c67790d5e55e966b4cdd74cade6c
Enquil <jim.e.olesen@gmail.com>
2023-01-25 02:27:16 +0000

    fix import statement for summernote

--- rtr/urls.py
+++ rtr/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('newssite.urls')),
     path('admin/', admin.site.urls),
-    path('summernote/', include('summernote.urls'))
+    path('summernote/', include('django_summernote.urls'))
 ]
