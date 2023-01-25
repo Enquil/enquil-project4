a0f518dd27e915d0f559388188dfed13153d5c87
Enquil <jim.e.olesen@gmail.com>
2023-01-25 16:42:22 +0000

    add some basic layout and navigte urls

--- newssite/views.py
+++ newssite/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
+from django.views import generic, View
+from .models import Post
 
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 4
